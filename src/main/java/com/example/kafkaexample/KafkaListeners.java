c2c825b0be0994ad75a273539850c8d1b6a7ef59
montser55 <majidmontser@gmail.com>
2022-05-01 14:08:30 +0600

    Initial commit

---
+++ src/main/java/com/example/kafkaexample/KafkaListeners.java
@@ -0,0 +1,16 @@
+package com.example.kafkaexample;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(
+            topics = "work_flow",
+            groupId = "groupId"
+    )
+    void listener(String data){
+        System.out.println("Listener received: "+ data +" :)" );
+    }
+}
